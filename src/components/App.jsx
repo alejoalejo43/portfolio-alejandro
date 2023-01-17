8633eaaa8555a6713915ecb250ac73ee910966a0
Oscar Alejandro Castillo Verano <55119670+alejoalejo43@users.noreply.github.com>
2023-01-16 22:37:40 -0500

    implementation header with links and dynamic background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
-import './App.scss';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Header from './layout/Header';
-import AboutMe from './pages/AboutMe';
-import ContactMe from './pages/ContactMe';
-import Cv from './pages/Cv';
-import Links from './pages/Links';
-import Portfolio from './pages/Portfolio';
-import Footer from './layout/Footer';
+import Home from '../pages/Home';
+import Header from '../layout/Header';
+import AboutMe from '../pages/AboutMe';
+import ContactMe from '../pages/ContactMe';
+import Cv from '../pages/Cv';
+import Links from '../pages/Links';
+import Portfolio from '../pages/Portfolio';
+import Footer from '../layout/Footer';
 
 function App() {
   return (
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,46 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-
+import logo from '../assets/firmaRS2.svg';
 
 const links = [
   {
     name: 'ABOUT ME',
     to: '/about',
     key: 'link-about',
+    background: '/src/assets/hero.jpg',
   },
   {
     name: 'CONTACT ME',
     to: '/contact',
     key: 'link-contact',
+    background: '/src/assets/hero.jpg',
   },
   {
     name: 'CV',
     to: '/cv',
     key: 'link-cv',
+    background: '/src/assets/hero.jpg',
   },
   {
     name: 'LINKS',
     to: '/links',
     key: 'link-links',
+    background: '/src/assets/hero.jpg',
   },
   {
     name: 'PORTFOLIO',
     to: '/portfolio',
     key: 'link-portfolio',
+    background: '/src/assets/hero_portfolio.jpg',
   },
 ];
 const Header = () => {
   const location = useLocation();
-  return (
-    <header>
+  const [background, setBackground] = useState('/src/assets/hero.jpg');
+  useEffect(() => {
+    const { pathname } = location;
+    const link = links.find((link) => link.to === pathname);
+    if (link) {
+      setBackground(link.background);
+    }
+  }, [location]);
 
+  return (
+    <header
+      className="hero"
+      style={{
+        backgroundImage: `
+        linear-gradient(
+          to right,
+          rgba(0, 0, 0, 0.6),
+          rgba(0, 0, 0, 0.6)
+        ),
+        url(${background})`,
+      }}
+    >
       <figure className="logo">
         <Link to={'/'}>
-          <img src="../assets/firmaRS2.svg" alt="logo" />
+          <img src={logo} alt="logo" />
         </Link>
       </figure>
 
-  
       <nav>
         <ul className="header_ul">
           {links.map((link) => {
